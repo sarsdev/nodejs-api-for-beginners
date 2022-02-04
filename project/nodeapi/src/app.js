541753d19c4d1b6c9824b7b781fde74c6743a82e
sarsdev <sarsdesenv@gmail.com>
2022-02-03 22:30:07 -0300

    Connecting to DataBase
    - Used DotEnv library
    - Used Mongoose library
    - Connected to MongoDb Atlas by the mongoose

--- project/nodeapi/src/app.js
+++ project/nodeapi/src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const postRoutes = require("./routes/post");
 
+dotenv.config();
 const app = express();
-const port = 8080;
+const port = process.env.PORT_API || 8080;
+const uriMongoDb = process.env.MONGODB_URI;
+
+mongoose.connect(uriMongoDb).then(() => console.info("DB Connected"));
+mongoose.connection.on("error", err => console.error(`DB connection error: ${err.message}`));
 
 app.use(morgan("dev"));
 app.use("/", postRoutes);
